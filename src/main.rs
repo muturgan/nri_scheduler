5b2e367b2afe78fa3cb9a9d06cc968927f4c46f1
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2024-12-24 14:48:58 +0300

    main func refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use ::std::sync::Arc;
+use ::std::{io::Error as IoError, sync::Arc};
 use nri_scheduler::{config, graceful_shutdown::shutdown_signal, repository::Repository, router};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), IoError> {
 	let repo = Repository::new().await;
 	let repo = Arc::new(repo);
 	let app = router::create_router(repo.clone());
 
 	let addr = config::get_http_host_to_serve();
-	let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+	let listener = tokio::net::TcpListener::bind(addr).await?;
 
 	println!(":) Server started successfully");
 
 	axum::serve(listener, app)
 		.with_graceful_shutdown(shutdown_signal(repo))
 		.await
-		.unwrap();
 }
