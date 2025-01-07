037e121cfef1d9a9e699a107c8b3e6437773f36a
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2025-01-07 22:20:29 +0300

    lazy VITE_PORT

--- server/vite/mod.rs
+++ server/vite/mod.rs
@@ -1,29 +1,35 @@
-use ::std::env::var as readEnvVar;
 use axum::{
 	BoxError,
 	body::Body,
 	http::{Request, StatusCode},
-	response::Response,
+	response::{IntoResponse, Response},
 };
+use lazy_static::lazy_static;
+use reqwest::Url;
+
+lazy_static! {
+	static ref VITE_PORT: u16 = ::std::env::var("VITE_PORT")
+		.expect("VITE_PORT environment variable is not defined")
+		.parse::<u16>()
+		.expect("VITE_PORT is not a correct u16");
+}
 
 pub async fn proxy_to_vite(req: Request<Body>) -> Response {
 	fetch_vite(req).await.unwrap_or_else(|err| {
-		Response::builder()
-			.status(StatusCode::INTERNAL_SERVER_ERROR)
-			.body(Body::from(err.to_string()))
-			.unwrap()
+		(
+			StatusCode::INTERNAL_SERVER_ERROR,
+			Body::from(err.to_string()),
+		)
+			.into_response()
 	})
 }
 
 async fn fetch_vite(req: Request<Body>) -> Result<Response, BoxError> {
-	let path = req.uri().path();
-
-	let vite_port = readEnvVar("VITE_PORT")
-		.expect("VITE_PORT environment variable is not defined")
-		.parse::<u16>()
-		.expect("VITE_PORT is not a correct u16");
-
-	let vite_url = format!("http://127.0.0.1:{vite_port}{path}");
+	let vite_url = Url::parse(&format!(
+		"http://127.0.0.1:{}{}",
+		*VITE_PORT,
+		req.uri().path()
+	))?;
 
 	let vite_res = reqwest::get(vite_url).await?;
 
