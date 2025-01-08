ed3dddc8dc138c114f664e519092b5ce2f999b07
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2025-01-08 14:01:30 +0300

    lets try

--- server/main.rs
+++ server/main.rs
@@ -4,6 +4,7 @@ use tokio::net::TcpListener as AsyncTcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), IoError> {
+	println!("lets try");
 	let repo = Repository::new().await;
 	let repo = Arc::new(repo);
 	let app = router::create_router(repo.clone());
